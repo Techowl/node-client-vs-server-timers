9d0eb86aa97fc7dabc91dae7a3f91126e3ffe523
Daniel Kimbel <dkkimbel@gmail.com>
2014-02-14 15:30:58 -0800

    Flesh out key methods

--- javascripts/buttons.js
+++ javascripts/buttons.js
@@ -1,7 +1,44 @@
 Buttons = {
+  $startStop: null,
+  $reset: null,
   init: function() {
-    $('button#start-stop').click(function() {
-      alert('clicked!')
+    this.$startStop = $('button#start-stop')
+    this.$reset = $('button#reset')
+    this.addEventListeners()
+  },
+
+  addEventListeners: function() {
+    Buttons.$startStop.click(function() {
+      // clientTimer.toggle()
+      // serverTimer.toggle()
+      Buttons.toggleStartStopAppearance()
     })
+    Buttons.$reset.click(function() {
+      // clientTimer.reset()
+      // clientServer.reset()
+      Buttons.toggleResetAppearance()
+    })
+  }
+
+  toggleStartStopAppearance: function() {
+    var buttonText = Buttons.$startStop.text()
+    if (buttonText == 'Start') {
+      Buttons.styleAsStart()
+    } else {
+      Buttons.styleAsStop()
+    }
+  },
+
+  toggleResetAppearance: function() {
+    // check whether timers are running
+    // based on that, toggle to or away from grayish appearance
+  },
+
+  styleAsStart: function() {
+
+  },
+
+  styleAsStop: function() {
+
   }
 }
