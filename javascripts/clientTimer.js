29c0619612420c92dfabf8d799a912c779177029
Daniel Kimbel <dkkimbel@gmail.com>
2014-02-14 17:21:37 -0800

    Fully implement ClientTimer methods

--- javascripts/clientTimer.js
+++ javascripts/clientTimer.js
@@ -1,3 +1,60 @@
 ClientTimer = {
-  running: false
+  time: 0, // milliseconds
+  startTime: null,
+  stopTime: null,
+  running: false,
+
+  init: function() {
+    setInterval(ClientTimer.output, 50)
+  },
+
+  start: function() {
+    ClientTimer.startTime = new Date()
+    ClientTimer.running = true
+  },
+
+  stop: function() {
+    ClientTimer.stopTime = new Date()
+    ClientTimer.time += ClientTimer.stopTime - ClientTimer.startTime
+    ClientTimer.running = false
+  },
+
+  reset: function() {
+    ClientTimer.time = 0
+    ClientTimer.startTime = new Date()
+  },
+
+  output: function() {
+    ClientTimer.updateTime()
+    var timeOutput = ClientTimer.getOutputTime()
+    $('#client-side .counter').text(timeOutput)
+  },
+
+  updateTime: function() {
+    if (ClientTimer.running) {
+      ClientTimer.stop()
+      ClientTimer.start()
+    }
+  },
+
+  getTimeDeciseconds: function() {
+    var timeDeciseconds = Math.round(ClientTimer.time / 100)
+    if (timeDeciseconds > 99999) {
+      timeDeciseconds = 99999
+    }
+    return timeDeciseconds
+  },
+
+  getOutputTime: function() {
+    var timeString, str
+    var timeDeciseconds = ClientTimer.getTimeDeciseconds()
+    if (timeDeciseconds <= 9) {
+      timeString = '0.' + String(timeDeciseconds)
+    } else {
+      str = String(timeDeciseconds)
+      timeString = str.slice(0, str.length - 1) + '.' + str[str.length - 1]
+    }
+    return timeString
+  }
+
 }
