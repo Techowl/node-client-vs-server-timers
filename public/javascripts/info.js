83415ef45031c86a1bc663b66f90cb24681cfeab
Daniel Kimbel <dkkimbel@gmail.com>
2014-02-18 23:39:01 -0800

    Build script to handle clicks and animations for informational section

---
+++ public/javascripts/info.js
@@ -0,0 +1,45 @@
+Info = {
+  $button: null,
+
+  init: function() {
+    this.$button = $('#info i')
+    this.$button.on('click', this.showHandler.bind(this));
+  },
+
+  showHandler: function() {
+    this.release();
+    this.expand();
+    setTimeout(function() {
+      Info.$button.on('click', Info.hideHandler.bind(Info));
+    }, 600);
+  },
+
+  hideHandler: function() {
+    this.release();
+    this.hideText();
+    setTimeout(function() {
+      Info.$button.on('click', Info.showHandler.bind(Info));
+    }, 600);
+  },
+
+  release: function() {
+    this.$button.off('click')
+  },
+
+  expand: function() {
+    $('div#info').animate({'width': '346px', 'height': '170px'}, 400, this.showText)
+  },
+
+  shrink: function() {
+    $('div#info').animate({'width': '37px', 'height': '37px'}, 400)
+  },
+
+  showText: function() {
+    $('#about').fadeIn(200)
+  },
+
+  hideText: function() {
+    $('#about').fadeOut(200)
+    setTimeout(this.shrink, 200)
+  }
+};
